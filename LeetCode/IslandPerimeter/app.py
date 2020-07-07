05b377612577673e860e71fe6c61a1ce36067db0
Austin Schaffer <schaffer.austin.t@gmail.com>
2020-07-07 14:08:54 -0400

    Revert "Light code golf in an attempt to improve leetcode score"
    This reverts commit e994ae9cba7d7c6d2f62d78820e50c5009695df6.

--- LeetCode/IslandPerimeter/app.py
+++ LeetCode/IslandPerimeter/app.py
@@ -3,42 +3,47 @@ import unittest
 
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        height = len(grid)
-        width = len(grid[0])
-        
-        for i in range(height):
-            for j in range(width):
-                if grid[i][j]:
-                    visited_coordinates = set()
-                    seeds_current_round = [(i,j)]
-                    perimeter = 0
+        return determine_perimeter(grid)
 
-                    while any(seeds_current_round):
-                        seeds_next_round = []
-                        for seed in seeds_current_round:
-                            if seed in visited_coordinates:
-                                continue
-                            visited_coordinates.add(seed)
+def determine_perimeter(map_: List[List[int]]) -> list:
+    def bfs_determine_perimeter(coordinate: tuple) -> int:
+        visited_coordinates = set()
+        seeds_current_round = [coordinate]
+        perimeter = 0
 
-                            for neighbor in generate_bordering_coordinates(seed):
-                                is_land = (
-                                    neighbor[0] >= 0 and
-                                    neighbor[0] < height and
-                                    neighbor[1] >= 0 and
-                                    neighbor[1] < width and
-                                    grid[neighbor[0]][neighbor[1]]
-                                )
+        while any(seeds_current_round):
+            seeds_next_round = []
+            for seed in seeds_current_round:
+                if seed in visited_coordinates:
+                    continue
+                visited_coordinates.add(seed)
 
-                                if not is_land:
-                                    perimeter += 1
-                                else:
-                                    seeds_next_round.append(neighbor)
+                for neighbor in generate_bordering_coordinates(seed):
+                    is_land = (
+                        neighbor[0] >= 0 and
+                        neighbor[0] < len(map_) and
+                        neighbor[1] >= 0 and
+                        neighbor[1] < len(map_[0]) and
+                        map_[neighbor[0]][neighbor[1]]
+                    )
 
-                        seeds_current_round = seeds_next_round
+                    if not is_land:
+                        perimeter += 1
+                    else:
+                        seeds_next_round.append(neighbor)
 
-                    return perimeter
+            seeds_current_round = seeds_next_round
 
-        return 0
+        return perimeter
+
+    # Find the first non-0 cell
+    for i, row in enumerate(map_):
+        for j, cell in enumerate(row):
+            if cell:
+                # Return perimeter starting from that cell.
+                return bfs_determine_perimeter((i,j))
+
+    return 0
 
 def generate_bordering_coordinates(
     coordinate: Tuple[int, int], include_diagonals=False
@@ -63,4 +68,4 @@ TEST_MAP = [
 
 class Tests(unittest.TestCase):
     def test_1(self):
-        self.assertEqual(Solution().islandPerimeter(TEST_MAP), 16)
+        self.assertEqual(determine_perimeter(TEST_MAP), 16)
