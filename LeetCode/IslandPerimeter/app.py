2ea27fd784cb063e26b8b5d1b6b28861df2b57bf
Austin Schaffer <schaffer.austin.t@gmail.com>
2020-07-07 14:18:03 -0400

    Removed internal function

--- LeetCode/IslandPerimeter/app.py
+++ LeetCode/IslandPerimeter/app.py
@@ -3,47 +3,45 @@ import unittest
 
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        return determine_perimeter(grid)
+        # Find the first non-0 cell
+        for i, row in enumerate(grid):
+            for j, cell in enumerate(row):
+                if cell:
+                    # Return perimeter starting from that cell.
+                    return determine_perimeter_bfs((i,j), grid)
 
-def determine_perimeter(map_: List[List[int]]) -> list:
-    def bfs_determine_perimeter(coordinate: tuple) -> int:
-        visited_coordinates = set()
-        seeds_current_round = [coordinate]
-        perimeter = 0
+    
+def determine_perimeter_bfs(coordinate, map_: List[List[int]]) -> list:
+    visited_coordinates = set()
+    seeds_current_round = [coordinate]
+    perimeter = 0
 
-        while any(seeds_current_round):
-            seeds_next_round = []
-            for seed in seeds_current_round:
-                if seed in visited_coordinates:
-                    continue
-                visited_coordinates.add(seed)
+    while any(seeds_current_round):
+        seeds_next_round = []
+        for seed in seeds_current_round:
+            if seed in visited_coordinates:
+                continue
+            visited_coordinates.add(seed)
 
-                for neighbor in generate_bordering_coordinates(seed):
-                    is_land = (
-                        neighbor[0] >= 0 and
-                        neighbor[0] < len(map_) and
-                        neighbor[1] >= 0 and
-                        neighbor[1] < len(map_[0]) and
-                        map_[neighbor[0]][neighbor[1]]
-                    )
+            for neighbor in generate_bordering_coordinates(seed):
+                is_land = (
+                    neighbor[0] >= 0 and
+                    neighbor[0] < len(map_) and
+                    neighbor[1] >= 0 and
+                    neighbor[1] < len(map_[0]) and
+                    map_[neighbor[0]][neighbor[1]]
+                )
 
-                    if not is_land:
-                        perimeter += 1
-                    else:
-                        seeds_next_round.append(neighbor)
+                if not is_land:
+                    perimeter += 1
+                else:
+                    seeds_next_round.append(neighbor)
 
-            seeds_current_round = seeds_next_round
+        seeds_current_round = seeds_next_round
 
-        return perimeter
+    return perimeter
 
-    # Find the first non-0 cell
-    for i, row in enumerate(map_):
-        for j, cell in enumerate(row):
-            if cell:
-                # Return perimeter starting from that cell.
-                return bfs_determine_perimeter((i,j))
 
-    return 0
 
 def generate_bordering_coordinates(
     coordinate: Tuple[int, int], include_diagonals=False
