28c6d7fd03882b10f389f546e141bbd407a97704
Austin Schaffer <schaffer.austin.t@gmail.com>
2020-07-08 10:54:51 -0400

    Docs, comments, addtl unit test

--- LeetCode/3Sum/app.py
+++ LeetCode/3Sum/app.py
@@ -14,9 +14,16 @@ class Solution:
 
         for index, value in enumerate(nums):
             for two_sum_result in two_sum(TARGET - value, nums, index + 1):
+                # Create a tuple from pinned value and sub-result. The values
+                # in this tuple will be ordered due to the list of nums being
+                # ordered and the outputs of `two_sum` being ordered.
                 result = (value, *two_sum_result)
+
+                # Make sure the current result is distinct.
                 if result in results_set:
                     continue
+
+                # Record the result in output format and "O(1) lookup" format
                 results_set.add(result)
                 results.append(list(result))
 
@@ -36,6 +43,7 @@ def two_sum(target: int, nums: List[int], start_index=0) -> Iterable[Tuple[int]]
         # the current value has already been seen. If so, yield it as a result.
         diff = (target - current_value)
         if diff in previous_values:
+            # Returned in this order to preserve input order, which should be sorted.
             yield (diff, current_value)
 
         # This value has now been seen before.
@@ -43,7 +51,13 @@ def two_sum(target: int, nums: List[int], start_index=0) -> Iterable[Tuple[int]]
 
 
 def cleanup_nums(max_each_num: int, nums: List[int]) -> List[int]:
+    """
+    Sorts the input list of numbers making sure that each number only appears
+    at most `max_each_num` times in the output.
+    """
+
     histogram = defaultdict(int)
+
     for num in nums:
         histogram[num] += 1
 
@@ -60,6 +74,16 @@ class Tests(unittest.TestCase):
         _output = [
             [-1, 0, 1],
             [-1, -1, 2],
+        ]
+
+        solution = Solution()
+        self.assertListEqual(solution.threeSum(_input), _output)
+
+    def test_2(self):
+        _input = [1, 0, -1] * 1000000
+        _output = [
+            [-1, 0, 1],
+            [0, 0, 0],
         ]
 
         solution = Solution()
