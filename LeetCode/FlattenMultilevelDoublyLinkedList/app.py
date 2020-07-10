3ae3bdbd589f93f128c939490deecd7e58a9f6a5
Austin Schaffer <schaffer.austin.t@gmail.com>
2020-07-10 16:41:59 -0400

    Added problem description for flattening linked list

---
+++ LeetCode/FlattenMultilevelDoublyLinkedList/app.py
@@ -0,0 +1,10 @@
+class _Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+
+class Solution:
+    def flatten(self, head: _Node) -> _Node:
+        return head
