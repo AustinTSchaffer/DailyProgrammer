c2c049803aa3f1d891073654112891595f636e8e
Austin Schaffer <schaffer.austin.t@gmail.com>
2020-07-23 09:10:38 -0400

    Added solution for SingleNumber3

---
+++ LeetCode/SingleNumber3/app.py
@@ -0,0 +1,9 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        num_freq = defaultdict(int)
+        for num in nums:
+            num_freq[num] += 1
+        return [ num for num in num_freq if num_freq[num] == 1 ]
