1e5bdb340748a3278e1dab57fb8f611f0d48ca08
Austin Schaffer <schaffer.austin.t@gmail.com>
2020-07-23 09:15:54 -0400

    Added alternate solution

--- LeetCode/SingleNumber3/app.py
+++ LeetCode/SingleNumber3/app.py
@@ -1,9 +1,13 @@
-from collections import defaultdict
+import collections
 from typing import List
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
-        num_freq = defaultdict(int)
+        num_freq = collections.defaultdict(int)
         for num in nums:
             num_freq[num] += 1
+        return [ num for num in num_freq if num_freq[num] == 1 ]
+
+    def singleNumberUsingCounter(self, nums: List[int]) -> List[int]:
+        num_freq = collections.Counter(nums)
         return [ num for num in num_freq if num_freq[num] == 1 ]
