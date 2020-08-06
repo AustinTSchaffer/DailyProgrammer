1c130978a541da7e74e5617bfa80763b26844d5e
Austin Schaffer <schaffer.austin.t@gmail.com>
2020-08-06 18:28:14 -0400

    Reorg

--- Rustlang.Org/03_2_datatypes/src/main.rs
+++ Rustlang.Org/03_2_datatypes/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let guess: u32 = "42".parse().expect("Not a number!");
+    let _guess: u32 = "42".parse().expect("Not a number!");
     // Doesn't work:
     // let guess = "42".parse().expect("Not a number!");
     println!("Hello, world!");
