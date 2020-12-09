05a62713d6924c0a82697c9208521a2b8908c72d
Austin Schaffer <schaffer.austin.t@gmail.com>
2020-12-09 16:10:47 -0500

    Day 9 part 2 completed

--- AdventOfCode/2020/day_09/solution.py
+++ AdventOfCode/2020/day_09/solution.py
@@ -10,8 +10,8 @@ import itertools
 
 def is_a_sum_of_2(numbers, sum_):
     combinations = itertools.combinations(numbers, 2)
-    for combination in combinations:
-        if sum(combination) == sum_:
+    for a, b in combinations:
+        if a + b == sum_ and a != b:
             return True
     return False
 
@@ -22,6 +22,29 @@ def _26wise(numbers):
             next(iterator, None)
     return zip(*iterators)
 
+bad_number = -1
 for *therest, thelast in _26wise(data):
     if not is_a_sum_of_2(therest, thelast):
-        print(thelast)
+        bad_number = thelast
+
+print("Part 1:", bad_number)
+
+
+# %%
+
+starting_index = 0
+length = 1
+
+while True:
+    slice_ = data[starting_index:length]
+    sum_  = sum(slice_)
+    if sum_ == bad_number:
+        print("Part 2:", min(slice_) + max(slice_))
+        break
+    elif sum_ > bad_number:
+        starting_index += 1
+        length -= 1
+    else:
+        length += 1
+
+# %%
