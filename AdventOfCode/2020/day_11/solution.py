a8ae71e8779ef0ce854386bffc9b81f8a67f87e8
Austin Schaffer <schaffer.austin.t@gmail.com>
2020-12-11 14:20:18 -0500

    Day 11 part 2 completed

--- AdventOfCode/2020/day_11/solution.py
+++ AdventOfCode/2020/day_11/solution.py
@@ -19,6 +19,12 @@ def copy_seat_layout(seat_layout):
 
 # %% Part 1
 
+def in_bounds(row, col, seat_layout):
+    return (
+        row >= 0 and row < len(seat_layout) and
+        col >= 0 and col < len(seat_layout[row])
+    )
+
 def get_neighbors(row, col, seat_layout):
     return [
         seat_layout[_row][_col]
@@ -26,12 +32,11 @@ def get_neighbors(row, col, seat_layout):
         for _col in range(col - 1, col + 2)
         if (
             (_row, _col) != (row, col) and
-            _row >= 0 and _row < len(seat_layout) and
-            _col >= 0 and _col < len(seat_layout[row])
+            in_bounds(_row, _col, seat_layout)
         )
     ]
 
-def simulate(seat_layout, get_neighbors_alg=get_neighbors):
+def simulate(seat_layout, get_neighbors_alg=get_neighbors, crowded_threshold=4):
     next_state = copy_seat_layout(seat_layout)
     anything_changed = True
 
@@ -42,13 +47,14 @@ def simulate(seat_layout, get_neighbors_alg=get_neighbors):
         anything_changed = False
         for row_index, row in enumerate(current_state):
             for col_index, seat in enumerate(row):
-                neighbors = get_neighbors_alg(row_index, col_index, current_state)
                 if seat == OPEN_SEAT:
+                    neighbors = get_neighbors_alg(row_index, col_index, current_state)
                     if neighbors.count(OCCUPIED) <= 0:
                         anything_changed = True
                         next_state[row_index][col_index] = OCCUPIED
                 elif seat == OCCUPIED:
-                    if neighbors.count(OCCUPIED) >= 4:
+                    neighbors = get_neighbors_alg(row_index, col_index, current_state)
+                    if neighbors.count(OCCUPIED) >= crowded_threshold:
                         anything_changed = True
                         next_state[row_index][col_index] = OPEN_SEAT
 
@@ -64,4 +70,31 @@ print("Part 1:", seats_occupied(simulate(seat_layout)))
 
 # %% Part 2
 
+def get_line_of_sight_neighbors(row, col, seat_layout):
+    los_neighbors = []
+    directions = [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ]
 
+    for d_row, d_col in directions:
+        _row, _col = row + d_row, col + d_col
+        while in_bounds(_row, _col, seat_layout):
+            value = seat_layout[_row][_col]
+            if value != FLOOR:
+                los_neighbors.append(value)
+                break
+
+            _row, _col = _row + d_row, _col + d_col
+
+    return los_neighbors
+
+print("Part 2:", seats_occupied(simulate(seat_layout, get_line_of_sight_neighbors, crowded_threshold=5)))
+
+# %%
