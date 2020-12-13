a5cf68a41d5145c97d2575ea8222533fc845bd99
Austin Schaffer <schaffer.austin.t@gmail.com>
2020-12-12 21:54:45 -0500

    consistency++?

--- AdventOfCode/2020/day_01/solution.py
+++ AdventOfCode/2020/day_01/solution.py
@@ -2,10 +2,12 @@
 
 print(
     "\n".join(
-        f"Part {n+1}: {result}" for n, result in
+        f"Part {n+1}: {result}"
+        for n, result in
         enumerate(
            __import__("math").prod(next(
-                combo for combo in
+                combo
+                for combo in
                 __import__("itertools").combinations([
                     int(expense.strip())
                     for expense in
@@ -13,7 +15,8 @@ print(
                 ], number_of_values)
                 if sum(combo) == 2020
             ))
-            for number_of_values in range(2, 4)
+            for number_of_values in
+            range(2, 4)
         )
     )
 )
