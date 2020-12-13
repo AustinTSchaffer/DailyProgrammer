ab5b97546e07a999cb3527cfe3b5c35bdb39135b
Austin Schaffer <schaffer.austin.t@gmail.com>
2020-12-12 21:18:30 -0500

    Day 12 part 2 completed
    Added testing to part 1

--- AdventOfCode/2020/day_12/solution.py
+++ AdventOfCode/2020/day_12/solution.py
@@ -6,6 +6,14 @@ directions = [
     open("directions.txt").readlines()
 ]
 
+test_directions = [
+    ("F", 10),
+    ("N", 3),
+    ("F", 7),
+    ("R", 90),
+    ("F", 11),
+]
+
 # %% Part 1
 
 from typing import Dict, Tuple, List
@@ -49,6 +57,47 @@ def simulate(directions: List[Tuple[str, int]]) -> Tuple[int, int, int]:
 
     return x, y, rotation
 
+x, y, _ = simulate(test_directions)
+assert (abs(x) + abs(y) == 25), f"Expected {25} actual {abs(x) + abs(y)}"
+
 x, y, _ = simulate(directions)
 print("Part 1:", abs(x) + abs(y))
 
+# %% Part 2
+
+def simulate_part_2(directions: List[Tuple[str, int]]) -> Tuple[int, int, int]:
+    ship_x, ship_y = 0, 0
+    waypoint_x, waypoint_y = 10, 1
+
+    for action, value in directions:
+        if action == "N":
+            waypoint_y += value
+        elif action == "E":
+            waypoint_x += value
+        elif action == "S":
+            waypoint_y -= value
+        elif action == "W":
+            waypoint_x -= value
+        elif action in "RL":
+            rotation = (-value if action == "L" else value) % 360
+            if rotation == 0:
+                pass
+            elif rotation == 90:
+                waypoint_x, waypoint_y = waypoint_y, -waypoint_x
+            elif rotation == 180:
+                waypoint_x, waypoint_y = -waypoint_x, -waypoint_y
+            elif rotation == 270:
+                waypoint_x, waypoint_y = -waypoint_y, waypoint_x
+            else:
+                raise ValueError(f"Invalid rotation value: {rotation}")
+        elif action == "F":
+            ship_x += waypoint_x * value
+            ship_y += waypoint_y * value
+
+    return ship_x, ship_y
+
+x, y = simulate_part_2(test_directions)
+assert (abs(x) + abs(y) == 286), f"Expected 286 Actual {abs(x) + abs(y)}"
+
+x, y = simulate_part_2(directions)
+print("Part 2:", abs(x) + abs(y))
