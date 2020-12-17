2b701cd86ef67ca4b001d05a316b3c9b3b42fb62
Austin Schaffer <schaffer.austin.t@gmail.com>
2020-12-17 15:20:23 -0500

    Implemented any-dimension solution

--- AdventOfCode/2020/day_17/solution.py
+++ AdventOfCode/2020/day_17/solution.py
@@ -16,7 +16,36 @@ puzzle_input_state = [
 ]
 
 import collections
-from typing import List, Dict, Set
+from typing import List, Dict, Set, Iterable
+
+def simulate_2d(state: List[List[int]], n: int) -> Set[tuple]:
+    currently_active_cells = {
+        (x, y)
+        for y, row in enumerate(state)
+        for x, value in enumerate(row)
+        if value == 1
+    }
+
+    for _ in range(n):
+
+        state_tracker = collections.defaultdict(int)
+        for coords in currently_active_cells:
+            for x in range(coords[0] - 1, coords[0] + 2):
+                for y in range(coords[1] - 1, coords[1] + 2):
+                    new_coords = (x, y)
+                    if new_coords != coords:
+                        state_tracker[new_coords] += 1
+
+        next_state = set()
+        for coords, num_neighbors in state_tracker.items():
+            if coords in currently_active_cells and num_neighbors in [2, 3]:
+                next_state.add(coords)
+            elif coords not in currently_active_cells and num_neighbors == 3:
+                next_state.add(coords)
+
+        currently_active_cells = next_state
+
+    return currently_active_cells
 
 def simulate_3d(state: List[List[int]], n: int) -> Set[tuple]:
     currently_active_cells = {
@@ -44,7 +73,7 @@ def simulate_3d(state: List[List[int]], n: int) -> Set[tuple]:
             elif coords not in currently_active_cells and num_neighbors == 3:
                 next_state.add(coords)
 
-        currently_active_cells = next_state            
+        currently_active_cells = next_state
 
     return currently_active_cells
 
@@ -75,17 +104,62 @@ def simulate_4d(state: List[List[int]], n: int) -> Set[tuple]:
             elif coords not in currently_active_cells and num_neighbors == 3:
                 next_state.add(coords)
 
-        currently_active_cells = next_state            
+        currently_active_cells = next_state
 
     return currently_active_cells
 
 
-test_1 = simulate_3d(test_state, 6)
+def simulate(state: List[List[int]], n: int, d: int) -> Set[tuple]:
+    currently_active_cells = {
+        (x, y, *(0 for _ in range(d-2)))
+        for y, row in enumerate(state)
+        for x, value in enumerate(row)
+        if value == 1
+    }
+
+    def neighboring_coords(coordinate: tuple) -> Iterable[tuple]:
+        def _neighboring_coords(index: int, coordinate: tuple) -> Iterable[tuple]:
+            if index >= len(coordinate):
+                yield ()
+            else:
+                yield from (
+                    _
+                    for tuple_ in _neighboring_coords(index + 1, coordinate)
+                    for _ in (
+                        (1 + coordinate[index], *tuple_),
+                        (0 + coordinate[index], *tuple_),
+                        (-1 + coordinate[index], *tuple_),
+                    )
+                    if _ != coordinate
+                )
+
+        return _neighboring_coords(0, coordinate)
+
+    for _ in range(n):
+
+        state_tracker = collections.defaultdict(int)
+        for coords in currently_active_cells:
+            for new_coord in neighboring_coords(coords):
+                if new_coord != coords:
+                    state_tracker[new_coord] += 1
+
+        next_state = set()
+        for coords, num_neighbors in state_tracker.items():
+            if coords in currently_active_cells and num_neighbors in [2, 3]:
+                next_state.add(coords)
+            elif coords not in currently_active_cells and num_neighbors == 3:
+                next_state.add(coords)
+
+        currently_active_cells = next_state
+
+    return currently_active_cells
+
+test_1 = simulate(state=test_state, n=6, d=3)
 assert len(test_1) == 112
-part_1 = simulate_3d(puzzle_input_state, 6)
+part_1 = simulate(state=puzzle_input_state, n=6, d=3)
 print("Part 1:", len(part_1))
 
-test_2 = simulate_4d(test_state, 6)
+test_2 = simulate(state=test_state, n=6, d=4)
 assert len(test_2) == 848
-part_2 = simulate_4d(puzzle_input_state, 6)
+part_2 = simulate(state=puzzle_input_state, n=6, d=4)
 print("Part 2:", len(part_2))
