c3e574b896b75963aecd193d0b1bf54f5a8b6626
Austin Schaffer <schaffer.austin.t@gmail.com>
2021-12-20 16:05:48 -0500

    sshhhhh

--- AdventOfCode/2021/download_problems.sh
+++ AdventOfCode/2021/download_problems.sh
@@ -6,7 +6,8 @@ PROBABLY_MAX_NUM_LINES=10000
 for day in $(seq -w 1 25); do
     day_no_leading_0=$(echo $day | sed 's/^0//')
     mkdir -p day_$day/
-    curl "https://adventofcode.com/2021/day/$day_no_leading_0" -H "$COOKIE_HEADER" \
+    echo "Downloading https://adventofcode.com/2021/day/$day_no_leading_0"
+    curl -s "https://adventofcode.com/2021/day/$day_no_leading_0" -H "$COOKIE_HEADER" \
         | egrep -A$PROBABLY_MAX_NUM_LINES -B0 '\<article\>.+' \
         | pandoc -f html -t commonmark \
         | egrep -B$PROBABLY_MAX_NUM_LINES -A0 'Both parts of this puzzle are complete' \
