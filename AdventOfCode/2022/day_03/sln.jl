8b8a504603410adc71a75e2244ef23d6622261c4
Austin Schaffer <schaffer.austin.t@gmail.com>
2022-12-03 11:09:21 -0500

    Better result streaming for part 2

--- AdventOfCode/2022/day_03/sln.jl
+++ AdventOfCode/2022/day_03/sln.jl
@@ -16,35 +16,30 @@ scoreLetter(c::Char) = islowercase(c) ?
 
 using IterTools
 
-# Split each string in half
-part1 = map(d -> map(identity, partition(d, div(length(d), 2))), data)
+# Split each string in half. The map(identity, ...) is to convert
+# the halved string back to an array. Iterators.map is used here
+# to create a lazy stream as opposed to creating a new list for
+# every step in the chain.
+part1 = Iterators.map(elf -> map(identity, partition(elf, div(length(elf), 2))), data)
 
-# Find the intersection for each pair of sets
-part1 = map(e -> intersect(e[1], e[2]), part1)
-
+# Find the intersection of each compartment.
 # Add up the sum of the scores of the letters for part 1
 part1 = sum(
     scoreLetter(letter)
-    for intersection in part1
+    for compartment in part1
+    for intersection in intersect(compartment[1], compartment[2])
     for letter in intersection
 )
 
 println("Part 1: ", part1)
 
 # Partition the elves into trios, in order, defining the elves' groups
-part2 = partition(data, 3)
-
 # Determine the badge ID for each elf group
-part2 = [
-    intersect(group[1], group[2], group[3])
-    for group in part2
-]
-
 # Sum up the badge scores
 part2 = sum(
     scoreLetter(badge)
-    for group in part2
-    for badge in group
+    for group in partition(data, 3)
+    for badge in intersect(group[1], group[2], group[3])
 )
 
 println("Part 2: ", part2)
