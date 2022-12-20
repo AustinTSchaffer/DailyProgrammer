6ed69273c6fcc75bc6e214b97d10c49352dfe795
Austin Schaffer <schaffer.austin.t@gmail.com>
2022-12-19 21:46:26 -0500

    Converted CartesianIndex solution to Tuple{Int,Int} solution. Still too slow

--- AdventOfCode/2022/day_17/sln.jl
+++ AdventOfCode/2022/day_17/sln.jl
@@ -1,27 +1,27 @@
-shapes::Vector{Vector{CartesianIndex}} = [
-    map(CartesianIndex, [
+shapes::Vector{Vector{Tuple{Int, Int}}} = [
+    [
         (0, 0), (1, 0), (2, 0), (3, 0)
-    ]),
-    map(CartesianIndex, [
+    ],
+    [
                 (1, 2),
         (0, 1), (1, 1), (2, 1),
                 (1, 0),
-    ]),
-    map(CartesianIndex, [
+    ],
+    [
                         (2, 2),
                         (2, 1),
         (0, 0), (1, 0), (2, 0),
-    ]),
-    map(CartesianIndex, [
+    ],
+    [
         (0, 0), (0, 1), (0, 2), (0, 3)
-    ]),
-    map(CartesianIndex, [
+    ],
+    [
         (0, 1), (1, 1),
         (0, 0), (1, 0),
-    ]),
+    ],
 ]
 
-function getNthShape(shapes::Vector{Vector{CartesianIndex}}, iter::Int)::Vector{CartesianIndex}
+function getNthShape(shapes::Vector{Vector{Tuple{Int, Int}}}, iter::Int)::Vector{Tuple{Int, Int}}
     return shapes[((iter - 1) % length(shapes)) + 1]
 end
 
@@ -29,33 +29,34 @@ function loadWind(filename::String)::Vector{Int}
     data = filter(c -> c != 0, map(c -> c == '>' ? 1 : c == '<' ? -1 : 0, collect(read(open(filename), String))))
 end
 
-function applyWind(shape::Vector{CartesianIndex}, windDirection::Int)::Vector{CartesianIndex}
-    offset::CartesianIndex = CartesianIndex(windDirection, 0)
-    return map(idx -> idx + offset, shape)
+function applyWind(shape::Vector{Tuple{Int, Int}}, windDirection::Int)::Vector{Tuple{Int, Int}}
+    return return [
+        (idx[1] + windDirection, idx[2])
+        for idx in shape
+    ]
 end
 
-function collision(shape::Vector{CartesianIndex}, rockPile::Set{CartesianIndex}, chamberWidth=7)::Bool
+function collision(shape::Vector{Tuple{Int, Int}}, rockPile::Set{Tuple{Int, Int}}, chamberWidth=7)::Bool
     return any(idx -> idx[1] <= 0 || idx[1] >= chamberWidth + 1, shape) || any(idx -> idx ∈ rockPile, shape)
 end
 
 gravityOffset = CartesianIndex(0, -1)
 
 function simulateShape!(
-    rockPile::Set{CartesianIndex},
-    highestRock::CartesianIndex,
-    shapes::Vector{Vector{CartesianIndex}},
+    rockPile::Set{Tuple{Int, Int}},
+    highestRock::Int,
+    shapes::Vector{Vector{Tuple{Int, Int}}},
     shapeIndex::Int,
     wind::Vector{Int},
-    windex::Int)::Tuple{Int, CartesianIndex}
+    windex::Int)::Tuple{Int, Int}
 
-    initialOffset = CartesianIndex(3, highestRock[2] + 4)
-    shape::Vector{CartesianIndex} = map(r -> r + initialOffset, getNthShape(shapes, shapeIndex))
+    shape = map(r -> (r[1] + 3, r[2] + highestRock + 4), getNthShape(shapes, shapeIndex))
 
     while true
-        movedShape::Vector{CartesianIndex} = applyWind(shape, wind[((windex - 1) % length(wind)) + 1])
+        movedShape = applyWind(shape, wind[((windex - 1) % length(wind)) + 1])
         windex += 1
         shape = collision(movedShape, rockPile) ? shape : movedShape
-        movedShape = map(idx -> idx + gravityOffset, shape)
+        movedShape = map(idx -> (idx[1], idx[2] - 1), shape)
 
         if collision(movedShape, rockPile)
             for idx in shape
@@ -63,8 +64,8 @@ function simulateShape!(
             end
 
             for idx in shape
-                if idx > highestRock
-                    highestRock = idx
+                if idx[2] > highestRock
+                    highestRock = idx[2]
                 end
             end
 
@@ -74,13 +75,19 @@ function simulateShape!(
         shape = movedShape
     end
 
-    if shapeIndex % 1000 == 0
+    if shapeIndex % 1_000_000 == 0
         print("Iteration: $shapeIndex. Pile size: $(length(rockPile)). ")
+    end
+
+    if shapeIndex % 1000 == 0
         for rock in rockPile
-            if (highestRock - rock)[2] >= 1_000
+            if (highestRock - rock[2]) >= 1_000
                 delete!(rockPile, rock)
             end
         end
+    end
+
+    if shapeIndex % 1_000_000 == 0
         println("New pile size: $(length(rockPile)). Highest rock: $(highestRock)")
     end
 
@@ -104,13 +111,13 @@ end
 wind = loadWind("input.txt")
 sampleWind = loadWind("sample_input.txt")
 
-rockPile = Set{CartesianIndex}()
+rockPile = Set{Tuple{Int, Int}}()
 for i in 1:7
-    push!(rockPile, CartesianIndex(i, 0))
+    push!(rockPile, (i, 0))
 end
 
 windex = 1
-highestRock = CartesianIndex(0, 0)
+highestRock = 0
 
 for shapeIndex in 1:1_000_000_000_000
     result = simulateShape!(
