bcda6b25b2982a3403cd0d7010cfd9bfb76e6a4b
Austin Schaffer <austin.schaffer@everactive.com>
2023-12-05 17:00:30 -0500

    Added template for python

---
+++ AdventOfCode/2023/boilerplate/sln.py
@@ -0,0 +1,21 @@
+import re
+import dataclasses
+
+def parse_input(filename: str):
+    ...
+
+def part_1(input):
+    ...
+
+def part_2(input):
+    ...
+
+if __name__ == '__main__':
+    input = parse_input('input.txt')
+    sample_input = parse_input('sample_input.txt')
+
+    print('Part 1 (sample):', part_1(sample_input))
+    print('Part 1:', part_1(input))
+
+    print('Part 2 (sample):', part_2(sample_input))
+    print('Part 2:', part_2(input))
