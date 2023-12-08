77f05b5bd660c27a9a2563de914ab4d94bfd16fc
Austin Schaffer <schaffer.austin.t@gmail.com>
2023-12-08 00:32:01 -0500

    Better solution for 2023.05

--- AdventOfCode/2023/day_05/sln.py
+++ AdventOfCode/2023/day_05/sln.py
@@ -18,22 +18,6 @@ class MappingRange:
             return None
         return (source - self.src_range_start) + self.dst_range_start
 
-@dataclasses.dataclass
-class SrcToDstMap:
-    range_info: list[MappingRange]
-
-    def map_source(self, source: int) -> int:
-        mapped_value = None
-
-        for range in self.range_info:
-            if (destination := range.get_dst(source)) is not None:
-                if mapped_value is None:
-                    mapped_value = destination
-                else:
-                    raise ValueError("Yes, this should return a list.")
-
-        return mapped_value if mapped_value is not None else source
-
 
 greedy_int_re = re.compile(r'\d+')
 src_to_dst_map_re = re.compile(r'(?P<dst_range_start>\d+) (?P<src_range_start>\d+) (?P<range_len>\d+)')
@@ -43,13 +27,7 @@ src_to_dst_map_re = re.compile(r'(?P<dst_range_start>\d+) (?P<src_range_start>\d
 @dataclasses.dataclass
 class Input:
     seeds: list[int]
-    seed_to_soil_map: SrcToDstMap
-    soil_to_fert_map: SrcToDstMap
-    fert_to_water_map: SrcToDstMap
-    water_to_light_map: SrcToDstMap
-    light_to_temp_map: SrcToDstMap
-    temp_to_humid_map: SrcToDstMap
-    humid_to_loc_map: SrcToDstMap
+    maps: list[list[MappingRange]]
 
     def total_numbers_to_consider(self) -> int:
         """
@@ -72,19 +50,15 @@ class Input:
         for range in self.seed_ranges():
             yield from (range[0], range[1])
 
+    def map_source(self, mapping_ranges: list[MappingRange], source: int) -> int:
+        for mapping_range in mapping_ranges:
+            if (destination := mapping_range.get_dst(source)) is not None:
+                return destination
+
+        return source
 
     def get_location(self, seed: int) -> int:
-        maps = [
-            self.seed_to_soil_map,
-            self.soil_to_fert_map,
-            self.fert_to_water_map,
-            self.water_to_light_map,
-            self.light_to_temp_map,
-            self.temp_to_humid_map,
-            self.humid_to_loc_map,
-        ]
-
-        return functools.reduce(lambda prev, map_: map_.map_source(prev), maps, seed)
+        return functools.reduce(lambda prev, map_: self.map_source(map_, prev), self.maps, seed)
 
 
 def parse_input(filename: str) -> Input:
@@ -102,7 +76,7 @@ def parse_input(filename: str) -> Input:
                     range_len=int(match_['range_len'])
                 ))
 
-        return Input(parsed_seeds, *map(SrcToDstMap, parsed_mapping_sections))
+        return Input(parsed_seeds, parsed_mapping_sections)
 
 def part_1(input: Input):
     min_ = None
@@ -112,7 +86,154 @@ def part_1(input: Input):
             min_ = location
     return min_
 
-def part_2(input: Input, num_samples_per_range = 100000) -> tuple[int, int]:
+
+def part_2(input: Input) -> int:
+    """
+    This algorithm iteratively passes entire seed ranges through entire mapping layers.
+    As it progresses through mapping layers, individual seed ranges will potentially be
+    split up into smaller ranges with differing offsets.
+
+    This algorithm works because all of the individual mapping ranges in the input
+    are of the form `f(x) = x + c`. Across an entire mapping layer, the function is not
+    contiguous.
+
+    To start, all of the seed ranges are initialized with a `c` value of 0. This signifies
+    that all of the seed values are currently passing through the function `f(x) = x`.
+
+    For each mapping layer, each of the ranges are checked against all of the maps to
+    see if there are any overlaps. Overlaps are determined using the output of each
+    range's current function. Any overlaps that are detected are stripped off of the
+    range, have their `c` values updated, and are then pushed down to the next layer.
+    The rest of the range is pushed back into the list of current functions being
+    evaluated. If a range does not overlap with any of the maps, then it is also
+    pushed down to the next layer without updating its output function.
+
+    The part described above can probably be slightly optimized if the maximum range
+    end of the layer is compared against the current function's lower bounds (and
+    vice-versa), as that would definitively mean that the function cannot overlap
+    with anything in the current layer.
+
+    At the end of this algorithm, the `current_functions` list will contain the full
+    function definition, aggregating the ranges and coefficients across all layers
+    into a single set of transformers of the form `f(x) = x + c`, each with
+    non-overlapping ranges.
+
+    For the actual part 2 question, we can add the coefficients and the function range
+    lower bounds to determine the minimum result that can be produced by that range.
+    Taking the minimum across all ranges results in the function's global minimum.
+    """
+
+    # List if linear functions.
+    #   0: Range start inclusive
+    #   1: Range start exclusive
+    #   2: Constant coefficient
+    current_functions = [(*range_, +0) for range_ in input.seed_ranges()]
+    for mapping_functions in input.maps:
+        function_layer = [
+            (
+                m.src_range_start,
+                m.src_range_start + m.range_len,
+                m.dst_range_start - m.src_range_start
+            )
+            for m in mapping_functions
+        ]
+
+        functions_next_round = set()
+        while len(current_functions) > 0:
+            func = current_functions.pop(-1)
+            found_overlap = False
+
+            # This is the output of the source range after it has passed through
+            # the layers above the current mapping ranges. This is the range
+            # of values that must be compared with the input ranges on the
+            # current layer.
+            offset_src = (func[0] + func[2], func[1] + func[2])
+
+            for layer_func in function_layer:
+                if offset_src[0] < layer_func[0]:
+                    if layer_func[0] < offset_src[1] <= layer_func[1]:
+                        # ...ssss
+                        #      mmmmm
+
+                        current_functions.append((
+                            func[0],
+                            func[0] + (layer_func[0] - offset_src[0]),
+                            func[2],
+                        ))
+
+                        functions_next_round.add((
+                            func[0] + (layer_func[0] - offset_src[0]),
+                            func[1],
+                            func[2] + layer_func[2]
+                        ))
+
+                        found_overlap = True
+                        break
+
+                    elif offset_src[1] > layer_func[1]:
+                        # ...sssssss...
+                        #      mmm
+                        current_functions.append((
+                            func[0],
+                            func[0] + (layer_func[0] - offset_src[0]),
+                            func[2]
+                        ))
+
+                        functions_next_round.add((
+                            func[0] + (layer_func[0] - offset_src[0]),
+                            func[0] + (layer_func[0] - offset_src[0]) + (layer_func[1] - layer_func[0]),
+                            func[2] + layer_func[2]
+                        ))
+
+                        current_functions.append((
+                            func[0] + (layer_func[0] - offset_src[0]) + (layer_func[1] - layer_func[0]),
+                            func[1],
+                            func[2]
+                        ))
+
+                        found_overlap = True
+                        break
+
+                elif layer_func[0] <= offset_src[0] < layer_func[1]:
+                    if offset_src[1] <= layer_func[1]:
+                        #   ssss(s)
+                        # mmmmmmmm
+                        functions_next_round.add((
+                            func[0],
+                            func[1],
+                            func[2] + layer_func[2],
+                        ))
+
+                        found_overlap = True
+                        break
+
+                    else:
+                        #    sssss...
+                        # mmmmmm
+                        functions_next_round.add((
+                            func[0],
+                            func[0] + (layer_func[1] - offset_src[0]),
+                            func[2] + layer_func[2]
+                        ))
+
+                        current_functions.append((
+                            func[0] + (layer_func[1] - offset_src[0]),
+                            func[1],
+                            func[2]
+                        ))
+
+                        found_overlap = True
+                        break
+
+            if not found_overlap:
+                functions_next_round.add(func)
+
+        current_functions = list(functions_next_round)
+
+    return min(r[0] + r[2] for r in current_functions), current_functions
+
+
+def part_2_sampling_and_climbing(input: Input, num_samples_per_range = 100000) -> tuple[int, int]:
     seed_ranges = input.seed_ranges()
 
     total_locations_calculated = 0
@@ -145,18 +266,20 @@ def part_2(input: Input, num_samples_per_range = 100000) -> tuple[int, int]:
 
     print("Searching backwards...")
 
-    current_seed = min_location_seed
-    while current_seed >= min_location_parent_range[0]:
-        current_seed -= 1
-        location = input.get_location(current_seed)
+    best_seed = min_location_seed
+    while best_seed >= min_location_parent_range[0]:
+        best_seed -= 1
+        location = input.get_location(best_seed)
         total_locations_calculated += 1
 
         if location > min_location:
+            best_seed += 1
             break
 
         min_location = location
 
-    return min_location, total_locations_calculated
+    return min_location, best_seed, min_location_parent_range, total_locations_calculated
+
 
 def analyze_input(input_: Input):
     seed_ranges = input_.seed_ranges()
@@ -234,11 +357,24 @@ if __name__ == '__main__':
     print('Part 1 (sample):', part_1(sample_input))
     print('Part 1:', part_1(input))
 
-    part_2_result = part_2(sample_input, 10)
-    print('Part 2 (sample):', part_2_result[0])
-    print(f'\tGenerated {part_2_result[1]:,} locations\n\tTotal seeds: {sample_input.total_numbers_to_consider():,}\n\tPercentage of seed space searched: {(part_2_result[1]/sample_input.total_numbers_to_consider())*100:.4}%')
+    result, _ = part_2(sample_input)
+    print('Part 2 (sample):', result)
 
-    part_2_result = part_2(input, 10_000)
-    print('Part 2:', part_2_result[0])
-    print(f'\tGenerated {part_2_result[1]:,} locations\n\tTotal seeds: {input.total_numbers_to_consider():,}\n\tPercentage of seed space searched: {(part_2_result[1]/input.total_numbers_to_consider())*100:.4}%')
+    result, function_ranges = part_2(input)
+    print('Part 2:', result)
 
+    print('f(x) = {')
+    for i, range in enumerate(sorted(function_ranges)):
+        function_def = '\tx'
+        if range[2] > 0:
+            function_def += f' + {range[2]}'
+        elif range[2] < 0:
+            function_def += f' - {-range[2]}'
+        
+        function_def += f' for {range[0]:,} <= x < {range[1]:,}'
+        if i < (len(function_ranges) - 1):
+            function_def += ', and'
+        else:
+            function_def += '.'
+        print(function_def)
+    print('}')
