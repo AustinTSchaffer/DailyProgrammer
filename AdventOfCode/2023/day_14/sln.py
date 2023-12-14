2c87e1c4bff08d631f08a75ed1ff892eb6b2f8e7
Austin Schaffer <austin.schaffer@everactive.com>
2023-12-14 16:44:57 -0500

    AoC 2023 Day 14 Part 1

--- AdventOfCode/2023/day_14/sln.py
+++ AdventOfCode/2023/day_14/sln.py
@@ -3,14 +3,51 @@ import dataclasses
 
 @dataclasses.dataclass
 class Input:
-    ...
+    width: int
+    height: int
+    spheres: list[tuple[int, int]]
+    cubes: set[tuple[int, int]]
 
 def parse_input(filename: str) -> Input:
+    cubes = set()
+    spheres = []
     with open(filename, 'r') as f:
-        ...
+        for i, row in enumerate(f):
+            for j, val in enumerate(row.strip()):
+                if val == '#':
+                    cubes.add((i, j))
+                elif val == 'O':
+                    spheres.append((i, j))
+
+    return Input(
+        cubes=cubes,
+        spheres=spheres,
+        height=i+1,
+        width=j+1,
+    )
+
 
 def part_1(input: Input):
-    ...
+    spheres_after_rolling = set()
+    for sphere in input.spheres:
+        for i in range(sphere[0], -2, -1):
+            if i == -1:
+                spheres_after_rolling.add((0, sphere[1]))
+                break
+
+            test_loc = (i, sphere[1])
+            if test_loc in spheres_after_rolling or test_loc in input.cubes:
+                if (i+1, sphere[1]) in spheres_after_rolling:
+                    ...
+                spheres_after_rolling.add((i+1, sphere[1]))
+                break
+
+    assert len(spheres_after_rolling) == len(input.spheres)
+
+    return sum(
+        input.height - s[0]
+        for s in spheres_after_rolling
+    )
 
 def part_2(input: Input):
     ...
