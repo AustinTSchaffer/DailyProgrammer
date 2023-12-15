56ccbaf1affe2d6c9e19cdb1191a6ce6e4e9d19f
Austin Schaffer <austin.schaffer@everactive.com>
2023-12-15 12:32:57 -0500

    Additional documentation and improved the "can_go_here" closure

--- AdventOfCode/2023/day_12/sln.py
+++ AdventOfCode/2023/day_12/sln.py
@@ -27,16 +27,39 @@ def parse_input(filename: str) -> list[BrokenSprings]:
 
 def num_valid_arrangements(info: BrokenSprings) -> int:
     known, lens = info.known, info.lens
-    def can_start_at(run_len: int, idx: int):
+    broken_spring_mask = [c=='#' for c in known]
+
+    def can_start_at(run_idx: int, run_len: int, idx: int):
+        """
+        Returns true if
+        - All of the indices of the length are in bounds.
+        - There are no broken springs directly before/after the length.
+        - There are no explicitly "good" springs contained within the length.
+
+        Additionally checks to make sure that the length doesn't exclude any broken
+        springs, in the event that the length is the first or last entry in `lens`.
+
+        TODO: This can absolutely be optimized. It redoes a lot of work when you pass
+        in a length at index 0, then again at index 1. This was not the most
+        inefficient part of the initial solution, and the economic incentive for
+        improving it is arguably worse than $0.
+        """
+
+        if run_idx == 0 and any(s for s in broken_spring_mask[0:idx] if s):
+            return False
+
+        if run_idx == (len(lens) - 1) and any(s for s in broken_spring_mask[idx+run_len:] if s):
+            return False
+
         if idx < 0 or (idx + run_len) > len(known):
             return False
 
         if (after := idx + run_len) < len(known):
-            if known[after] == '#':
+            if broken_spring_mask[after]:
                 return False
 
         if (before := idx - 1) >= 0:
-            if known[before] == '#':
+            if broken_spring_mask[before]:
                 return False
 
         for i in range(run_len):
@@ -45,17 +68,22 @@ def num_valid_arrangements(info: BrokenSprings) -> int:
 
         return True
 
+    # The amount of "flex" that the lengths have within the
+    # map of springs, based on putting at least a gap of 1
+    # between all of the lengths.
     wiggle_room = len(known) - sum(lens) - (len(lens) - 1)
     if wiggle_room < 0:
         raise ValueError(f"{known} {lens}")
 
+    # Trivial case.
     if wiggle_room == 0:
         return 1
 
-    minimum_starts = [None] * len(lens)
-    allowed_positions = [None] * len(lens)
-
-    print("Determining minimum starting locations.")
+    # Collects the absolute minimum starting locations for each
+    # of the lengths, plus a list of lists, recording all of the
+    # positions where each of the lengths _could_ be.
+    minimum_starts: list[int] = [None] * len(lens)
+    allowed_positions: list[list[int]] = [None] * len(lens)
 
     for i, val in enumerate(lens):
         if i == 0:
@@ -65,7 +93,7 @@ def num_valid_arrangements(info: BrokenSprings) -> int:
 
         allowed_positions[i] = []
         for j in range(wiggle_room+1):
-            if can_start_at(val, x := minimum_starts[i] + j):
+            if can_start_at(i, val, x := minimum_starts[i] + j):
                 allowed_positions[i].append(x)
 
     # For each allowed position for each continuous run of broken springs, make sure that there are no overlaps.
@@ -118,7 +146,7 @@ if __name__ == '__main__':
     sample_input = parse_input('sample_input.txt')
 
     print('Part 1 (sample, x=21):', part_1(sample_input))
-    print('Part 1 (5822 < x < 9029):', part_1(input))
+    print('Part 1 (x=7236):', part_1(input))
 
     print('Part 2 (sample):', part_2(sample_input))
     print('Part 2:', part_2(input))
