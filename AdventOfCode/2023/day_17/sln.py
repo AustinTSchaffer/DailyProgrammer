9a6940a7ecaa75505794759885c796cd5b0f84a9
Austin Schaffer <austin.schaffer@everactive.com>
2023-12-18 13:18:07 -0500

    efficiency increase for AoC 2023 `day 17

--- AdventOfCode/2023/day_17/sln.py
+++ AdventOfCode/2023/day_17/sln.py
@@ -1,7 +1,7 @@
 import re
 import dataclasses
 import timeit
-from typing import Iterable
+from typing import Iterable, NamedTuple
 import astar
 import math
 
@@ -23,12 +23,28 @@ def parse_input(filename: str) -> Input:
                 heatmap[-1].append(int(val))
 
         return Input(
-            start=(0,0),
-            end=(i,j),
+            start=(0, 0),
+            end=(i, j),
             heatmap=heatmap,
             width=j+1,
             height=i+1,
         )
+
+
+def print_heatmap(input: Input, highlight: list[tuple[int, int]] = ()):
+    result = []
+    for i, row in enumerate(input.heatmap):
+        for j, val in enumerate(row):
+            if (i, j) in highlight:
+                result.append(f'\033[1m{val}\033[0m')
+            else:
+                result.append(str(val))
+        result.append('\n')
+    print(''.join(result))
+
+
+TNode = tuple[int, int, int, int]
+
 
 class AStar_Day17(astar.AStar):
     def __init__(self, input: Input):
@@ -36,134 +52,97 @@ class AStar_Day17(astar.AStar):
 
     _diffs = ((0, -1), (0, +1), (-1, 0), (+1, 0))
 
-    def gen_neighbors(self, node: tuple[tuple[int, int], ...]) -> tuple[int, int]:
+    def neighbors(self, node: TNode) -> Iterable[TNode]:
         for diff in AStar_Day17._diffs:
             # Can't go out of bounds.
-            if (i := node[-1][0] + diff[0]) < 0 or i >= self.input.height:
+            if (i := node[0] + diff[0]) < 0 or i >= self.input.height:
                 continue
 
             # Can't go out of bounds.
-            if (j := node[-1][1] + diff[1]) < 0 or j >= self.input.width:
+            if (j := node[1] + diff[1]) < 0 or j >= self.input.width:
                 continue
-
-            tup = (i, j)
 
             # Can't reverse direction.
-            if len(node) > 1 and tup == node[-2]:
+            if (diff[0] and ((diff[0] * node[2]) < 0)) or (diff[1] and ((diff[1] * node[3]) < 0)):
                 continue
 
-            yield tup
+            yield (
+                i,
+                j,
+                (node[2] + diff[0]) if diff[0] else 0,
+                (node[3] + diff[1]) if diff[1] else 0,
+            )
 
-    def distance_between(self, _, n2: tuple[tuple[int, int], ...]) -> float:
-        i, j = n2[-1]
-        return self.input.heatmap[i][j]
+    def distance_between(self, _, n2: TNode) -> float:
+        return self.input.heatmap[n2[0]][n2[1]]
 
-    def is_goal_reached(self, current: tuple[tuple[int, int], ...], goal: tuple[int, int]) -> bool:
-        return current[-1] == goal
+    def is_goal_reached(self, current: TNode, goal: tuple[int, int]) -> bool:
+        return current[0] == goal[0] and current[1] == goal[1]
     
-    def heuristic_cost_estimate(self, current: tuple[tuple[int, int], ...], goal: tuple[int, int]) -> float:
-        return abs(goal[0] - current[-1][0]) + abs(goal[1] - current[-1][1])
+    def heuristic_cost_estimate(self, current: TNode, goal: tuple[int, int]) -> float:
+        return abs(goal[0] - current[0]) + abs(goal[1] - current[1])
 
 
 class HeatmapAstarPart1(AStar_Day17):
     def __init__(self, input: Input):
         super().__init__(input)
 
-    def neighbors(self, node: tuple[tuple[int, int], ...]) -> Iterable:
-        for neighbor in self.gen_neighbors(node):
-            i, j = neighbor
-
-            history = len(node)
-            if history <= 3:
-                yield (*node, neighbor)
-            else:
-                if all(i==n[0] for n in node):
-                    continue
-                if all(j==n[1] for n in node):
-                    continue
-                yield (*node[1:], (i, j))
-
-
-def part_1(input: Input):
-    result = HeatmapAstarPart1(input).astar(((input.start,)), input.end)
-    result = list(result)
-    return sum(
-        input.heatmap[node[-1][0]][node[-1][1]]
-        for node in result[1:]
-    )
+    def neighbors(self, node: TNode) -> Iterable[TNode]:
+        for neighbor in super().neighbors(node):
+            if abs(neighbor[2]) <= 3 and abs(neighbor[3]) <= 3:
+                yield neighbor
 
 class HeatmapAstarPart2(AStar_Day17):
     def __init__(self, input: Input):
         super().__init__(input)
 
-    def neighbors(self, node: tuple[tuple[int, int], ...]) -> Iterable:
-        valid_neighbors = []
-        for neighbor in self.gen_neighbors(node):
-            i, j = neighbor
-
+    def neighbors(self, node: TNode) -> TNode:
+        for neighbor in super().neighbors(node):
             # Invalidate early turns
-            relevant_nodes = node[-5:]
-            neighbor_requires_invalid_turn = (len(node) > 1 and i != node[-2][0] and j != node[-2][1]) and (
-                (
-                    len(relevant_nodes) < 5 and not (
-                        all(i == n[0] for n in relevant_nodes) or
-                        all(j == n[1] for n in relevant_nodes)
-                    )
-                ) or (
-                    len(relevant_nodes) == 5 and not (
-                        all(relevant_nodes[0][0] == n[0] for n in relevant_nodes) or
-                        all(relevant_nodes[0][1] == n[1] for n in relevant_nodes)
-                    )
-                )
+            neighbor_requires_invalid_turn = (
+                (node[2] and abs(node[2]) < 4 and not neighbor[2]) or
+                (node[3] and abs(node[3]) < 4 and not neighbor[3])
             )
 
             if neighbor_requires_invalid_turn:
                 continue
 
             # Invalidate long runs.
-            relevant_nodes = node[-11:]
-            neighbor_in_a_run_of_11 = (
-                len(relevant_nodes) == 11 and (
-                    all(i == n[0] for n in relevant_nodes) or
-                    all(j == n[1] for n in relevant_nodes)
-                )
-            )
-
-            if neighbor_in_a_run_of_11:
+            if abs(neighbor[2]) > 10 or abs(neighbor[3]) > 10:
                 continue
 
-            valid_neighbors.append((*node[-10:], neighbor))
-        return valid_neighbors
+            yield neighbor
     
-    def is_goal_reached(self, current: tuple[tuple[int, int], ...], goal: tuple[int, int]) -> bool:
-        last_5 = current[-5:]
+    def is_goal_reached(self, current: TNode, goal: tuple[int, int]) -> bool:
         return (
             super().is_goal_reached(current, goal) and (
-                all(last_5[0][0] == n[0] for n in last_5) or
-                all(last_5[0][1] == n[1] for n in last_5)
+                abs(current[2]) >= 4 or abs(current[3]) >= 4
             )
         )
 
-def print_heatmap(input: Input, highlight: list[tuple[int, int]] = ()):
-    result = []
-    for i, row in enumerate(input.heatmap):
-        for j, val in enumerate(row):
-            if (i,j) in highlight:
-                result.append(f'\033[1m{val}\033[0m')
-            else:
-                result.append(str(val))
-        result.append('\n')
-    print(''.join(result))
 
-def part_2(input: Input, print_route=False):
-    result = HeatmapAstarPart2(input).astar(((input.start,)), input.end)
+def part_1(input: Input, print_route=True):
+    result = HeatmapAstarPart1(input).astar((*input.start, 0, 0), input.end)
     result = list(result)
 
     if print_route:
-        print_heatmap(input, [n[-1] for n in result])
+        print_heatmap(input, {(n[0], n[1]) for n in result})
 
     return sum(
-        input.heatmap[node[-1][0]][node[-1][1]]
+        input.heatmap[node[0]][node[1]]
+        for node in result[1:]
+    )
+
+
+def part_2(input: Input, print_route=True):
+    result = HeatmapAstarPart2(input).astar((*input.start, 0, 0), input.end)
+    result = list(result)
+
+    if print_route:
+        print_heatmap(input, {(n[0], n[1]) for n in result})
+
+    return sum(
+        input.heatmap[node[0]][node[1]]
         for node in result[1:]
     )
 
