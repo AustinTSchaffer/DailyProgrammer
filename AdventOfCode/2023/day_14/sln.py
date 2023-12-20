a0173b3ddbc4ca877109f737073f0bd77656aaef
Austin Schaffer <austin.schaffer@everactive.com>
2023-12-20 15:33:00 -0500

    Benchmarking various solutions

--- AdventOfCode/2023/day_14/sln.py
+++ AdventOfCode/2023/day_14/sln.py
@@ -51,7 +51,62 @@ def part_1(input: Input):
         for s in spheres_after_rolling
     )
 
-def part_2(input: Input, total_iterations=1_000_000_000):
+
+def part_2_sets(input: Input, total_iterations=1_000_000_000):
+    # Roll the boulders North, West, South, East iteratively.
+    # Store data as to check for a cycle.
+    #    - Store post shifting results as a tuple of tuples and hash them?
+    #    - Use a dict for the tup-o-tups, along with instruction index, map to iteration number?
+    # Figure out where in the cycle 1_000_000_000 repetitions will be.
+    # Calculate load.
+    history: dict[tuple[tuple[int, int], ...], int] | dict[int, tuple[tuple[int, int], ...]] = {}
+    current_state = tuple(sorted(input.spheres))
+
+    rotated_cubes_maps: list[set] = [set(sorted(input.cubes)), set(), set(), set()]
+
+    for iter_, (source_cm, dest_cm) in enumerate(itertools.pairwise(rotated_cubes_maps)):
+        for i, j in source_cm:
+            dest_cm.add((j, (input.width if iter_ % 2 == 0 else input.height) - 1 - i))
+
+    for current_iteration in range(total_iterations):
+        if (repeated_iteration := history.get(current_state)) is not None:
+            cycle_length = current_iteration - repeated_iteration
+
+            # Lookup what the current state will be at iteration (1B-1)
+            ending_iteration = ((total_iterations - current_iteration) % cycle_length) + repeated_iteration
+            final_state = history[ending_iteration]
+
+            return sum(
+                input.height - s[0]
+                for s in final_state
+            )
+
+        history[current_iteration] = current_state
+        history[current_state] = current_iteration
+
+        for instruction in range(4):
+            cube_map = rotated_cubes_maps[instruction]
+
+            spheres_after_rolling = set()
+            for sphere in current_state:
+                for i in range(sphere[0], -2, -1):
+                    if i == -1:
+                        spheres_after_rolling.add((0, sphere[1]))
+                        break
+
+                    test_loc = (i, sphere[1])
+                    if test_loc in spheres_after_rolling or test_loc in cube_map:
+                        spheres_after_rolling.add((i+1, sphere[1]))
+                        break
+
+            spheres_after_rolling_rotated = [None] * len(spheres_after_rolling)
+            for idx, (i, j) in enumerate(spheres_after_rolling):
+                spheres_after_rolling_rotated[idx] = (j, (input.width if instruction % 2 == 0 else input.height) - 1 - i)
+
+            current_state = tuple(sorted(spheres_after_rolling_rotated))
+
+
+def part_2_1d_arrays(input: Input, total_iterations=1_000_000_000):
     # Roll the boulders North, West, South, East iteratively.
     # Store data as to check for a cycle.
     #    - Store post shifting results as a tuple of tuples and hash them?
@@ -124,28 +179,36 @@ def part_2(input: Input, total_iterations=1_000_000_000):
             current_state = current_state_temp
         current_state = tuple(current_state)
 
+
 if __name__ == '__main__':
     input = parse_input('input.txt')
     sample_input = parse_input('sample_input.txt')
     tiny_input = parse_input('tiny_input.txt')
 
-    globals = {'input': input, 'part_1': part_1, 'part_2': part_2}
+    timeit_globals = {'input': input, 'part_1': part_1, 'part_2': part_2_1d_arrays}
 
     part_1_timer = timeit.Timer(
         'global result; result = part_1(input)',
-        globals = globals
+        globals = timeit_globals
     )
 
     part_2_timer = timeit.Timer(
         'global result; result = part_2(input)',
-        globals = globals
+        globals = timeit_globals
     )
 
     print('Part 1 (sample):', part_1(sample_input))
     time = part_1_timer.timeit(1)
-    print('Part 1:', globals['result'], f'({time:.3} seconds)')
+    print('Part 1:', timeit_globals['result'], f'({time:.3} seconds)')
 
     # print('Part 2 (tiny):', part_2(tiny_input))
-    print('Part 2 (sample):', part_2(sample_input))
+    print('Part 2 (sample, sets):', part_2_sets(sample_input))
+    print('Part 2 (sample, 1D arrays):', part_2_1d_arrays(sample_input))
+
+    timeit_globals['part_2'] = part_2_sets
     time = part_2_timer.timeit(1)
-    print('Part 2:', globals['result'], f'({time:.3} seconds)')
+    print('Part 2 (sets):', timeit_globals['result'], f'({time:.3} seconds)')
+
+    timeit_globals['part_2'] = part_2_1d_arrays
+    time = part_2_timer.timeit(1)
+    print('Part 2 (1D arrays):', timeit_globals['result'], f'({time:.3} seconds)')
