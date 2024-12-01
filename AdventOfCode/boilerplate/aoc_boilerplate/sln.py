b27e66f236386943dc09d62f7ea311f99b82ce6d
Austin Schaffer <schaffer.austin.t@gmail.com>
2024-12-01 09:22:06 -0500

    Better boilerplate

--- AdventOfCode/boilerplate/aoc_boilerplate/sln.py
+++ AdventOfCode/boilerplate/aoc_boilerplate/sln.py
@@ -1,10 +1,15 @@
 import re
 import dataclasses
-import timeit
+import time
+from typing import Any
 
-@dataclasses.dataclass
-class Input:
-    ...
+# @dataclasses.dataclass
+# class Input:
+#     ...
+
+# Input = list
+
+Input = Any
 
 def parse_input(filename: str) -> Input:
     with open(filename, 'r') as f:
@@ -20,30 +25,26 @@ if __name__ == '__main__':
     input = parse_input('input.txt')
     sample_input = parse_input('sample_input.txt')
 
-    timeit_globals = {'input': input, 'part_1': part_1, 'part_2': part_2}
+    before = time.time_ns()
+    result = part_1(sample_input)
+    time_ms = (time.time_ns() - before) / 1000
 
-    part_1_timer = timeit.Timer(
-        'global result; result = part_1(input)',
-        globals=timeit_globals
-    )
+    print('Part 1 (sample):', result, f'({time_ms} ms)')
 
-    part_2_timer = timeit.Timer(
-        'global result; result = part_2(input)',
-        globals=timeit_globals
-    )
+    before = time.time_ns()
+    result = part_1(input)
+    time_ms = (time.time_ns() - before) / 1000
 
-    timeit_globals['input'] = sample_input
-    time = part_1_timer.timeit(1)
-    print('Part 1 (sample):', timeit_globals['result'], f'({time:.3} seconds)')
+    print('Part 1:', result, f'({time_ms} ms)')
 
-    timeit_globals['input'] = input
-    time = part_1_timer.timeit(1)
-    print('Part 1:', timeit_globals['result'], f'({time:.3} seconds)')
+    before = time.time_ns()
+    result = part_2(sample_input)
+    time_ms = (time.time_ns() - before) / 1000
 
-    timeit_globals['input'] = sample_input
-    time = part_2_timer.timeit(1)
-    print('Part 2 (sample):', timeit_globals['result'], f'({time:.3} seconds)')
+    print('Part 2 (sample):', result, f'({time_ms} ms)')
 
-    timeit_globals['input'] = input
-    time = part_2_timer.timeit(1)
-    print('Part 2:', timeit_globals['result'], f'({time:.3} seconds)')
+    before = time.time_ns()
+    result = part_2(input)
+    time_ms = (time.time_ns() - before) / 1000
+
+    print('Part 2:', result, f'({time_ms} ms)')
