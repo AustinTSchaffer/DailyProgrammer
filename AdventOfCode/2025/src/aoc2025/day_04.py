42981424be44a433c48997c6a7ae60c3219504c6
Austin Schaffer <aschaffer@sitetechnologies.io>
2025-12-19 11:46:37 -0500

    Caching for bounds-checked adjacencies

--- AdventOfCode/2025/src/aoc2025/day_04.py
+++ AdventOfCode/2025/src/aoc2025/day_04.py
@@ -18,8 +18,17 @@ offsets = [
     (+1, +1),
 ]
 
+_adj_loc_cache = {}
+def adj_locations(cache_key: int, input: list[list[bool]], row: int, col: int) -> list[tuple[int, int]]:
+    if cache_key in _adj_loc_cache:
+        input_adj_cache = _adj_loc_cache[cache_key]
+    else:
+        input_adj_cache = [[None for _ in range(len(input[row]))] for row in range(len(input))]
+        _adj_loc_cache[cache_key] = input_adj_cache
 
-def adj_locations(input: list[list[bool]], row: int, col: int) -> list[tuple[int, int]]:
+    if input_adj_cache[row][col]:
+        return input_adj_cache[row][col]
+
     _adj_locations = []
     for row_off, col_off in offsets:
         adj_row = row + row_off
@@ -35,13 +44,14 @@ def adj_locations(input: list[list[bool]], row: int, col: int) -> list[tuple[int
         if in_bounds:
             _adj_locations.append((adj_row, adj_col))
 
+    input_adj_cache[row][col] = _adj_locations
     return _adj_locations
 
 
-def num_adj_rolls(input: list[list[bool]], row: int, col: int) -> int:
+def num_adj_rolls(cache_key: int, input: list[list[bool]], row: int, col: int) -> int:
     count = 0
 
-    for adj_row, adj_col in adj_locations(input, row, col):
+    for adj_row, adj_col in adj_locations(cache_key, input, row, col):
         if input[adj_row][adj_col]:
             count += 1
 
@@ -49,16 +59,18 @@ def num_adj_rolls(input: list[list[bool]], row: int, col: int) -> int:
 
 
 def part_1(input: list[list[bool]]):
+    cache_key = id(input)
     accessible_rolls = 0
     for row_idx, row in enumerate(input):
         for col_idx, is_roll in enumerate(row):
-            if is_roll and (num_adj_rolls(input, row_idx, col_idx) < 4):
+            if is_roll and (num_adj_rolls(cache_key, input, row_idx, col_idx) < 4):
                 accessible_rolls += 1
 
     return accessible_rolls
 
 
 def part_2(input: list[list[bool]]):
+    cache_key = id(input)
     total_rolls_removed = 0
 
     # Placeholder value to get the ball rolling.
@@ -78,15 +90,15 @@ def part_2(input: list[list[bool]]):
                 for col_idx, is_roll in enumerate(row):
                     if not is_roll:
                         continue
-                    if num_adj_rolls(this_round, row_idx, col_idx) < 4:
+                    if num_adj_rolls(cache_key, this_round, row_idx, col_idx) < 4:
                         next_round[row_idx][col_idx] = False
                         rolls_removed_this_round.add((row_idx, col_idx))
 
         else:
             for row_idx, col_idx in rolls_removed_last_round:
-                for adj_loc in adj_locations(this_round, row_idx, col_idx):
+                for adj_loc in adj_locations(cache_key, this_round, row_idx, col_idx):
                     adj_row_idx, adj_col_idx = adj_loc
-                    if this_round[adj_row_idx][adj_col_idx] and (num_adj_rolls(this_round, adj_row_idx, adj_col_idx) < 4):
+                    if this_round[adj_row_idx][adj_col_idx] and (num_adj_rolls(cache_key, this_round, adj_row_idx, adj_col_idx) < 4):
                         next_round[adj_row_idx][adj_col_idx] = False
                         rolls_removed_this_round.add(adj_loc)
 
